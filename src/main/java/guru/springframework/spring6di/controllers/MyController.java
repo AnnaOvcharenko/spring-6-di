7bed81a34f03d50c2a8fac2814166ebb33c4ad18
Anna Ovcharenko <annaowczarenko@gmail.com>
2023-12-04 18:27:06 +0100

    30. The Spring Context and 31. Spring Test Context

---
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Anna Ovcharenko
+ */
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("I'm in the controller");
+        return "Hello everyone!!!";
+    }
+}
